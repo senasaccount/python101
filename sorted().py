d2650c0d10f70792c98ff9a83f873445e0a58136
senasaccount <56741176+senasaccount@users.noreply.github.com>
2021-07-02 22:51:01 +0300

    Add files via upload

---
+++ sorted().py
@@ -0,0 +1,7 @@
+unsorted_list = [("w",23), (9,1), ("543",99), ("sena",18)]
+
+print(sorted(unsorted_list, key=lambda x: x[1]))
+
+list = [43, 743, 342, 8874, 49]
+
+print(sorted(list, reverse=True))
