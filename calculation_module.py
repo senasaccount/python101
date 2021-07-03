1ada7212658f2301f217ee186ba827e2e2a0fc0c
senasaccount <56741176+senasaccount@users.noreply.github.com>
2021-07-03 11:10:47 +0300

    Add files via upload

---
+++ calculation_module.py
@@ -0,0 +1,6 @@
+#this file is created for the file using_calculation_module.py
+
+def new_salary(x):
+    print(int(x*6/5))
+
+salaries=[1000,2000,3000,4000]
---
+++ using_calculation_module.py
@@ -0,0 +1,7 @@
+#this code runs with the file calculation_module.py
+
+import calculation_module as cm
+
+cm.new_salary(3000)
+
+print(cm.salaries)
